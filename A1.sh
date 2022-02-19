7a33e49a8b68561056b3c7c24624bfb5d1b81af1
Mike Edwards <82988362+mike90s15@users.noreply.github.com>
2022-02-19 03:04:47 -0300

    Update A1.sh

--- A1.sh
+++ A1.sh
@@ -1,10 +1,12 @@
 while :; do
    if command -v php; then
        php -S localhost:8181 &> /dev/null &
-       exit 0
+       printf "\ec\n Organização Clownters 🇮🇲\e[m"
+       read 
    else
        printf "\e[1;34m Instalando PHP...\e[m"
        pkg i -y php
        [[ $? -eq 0 ]] || exit 1
    fi
 done
+exit 0
